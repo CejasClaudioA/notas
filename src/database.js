8c029c66d2845375bd7473ecd20f58f2d716e7a2
Cejas Claudio A <41306913+CejasClaudioA@users.noreply.github.com>
2020-08-01 02:16:20 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -4,9 +4,7 @@ const {NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE} = process.env;
 
 const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
 
-const mongodb_uri= "mongodb+srv://gm:gm2020@gm-ny80m.gcp.mongodb.net/Notas?retryWrites=true&w=majority";
-
-mongoose.connect(mongodb_uri,{
+mongoose.connect(MONGODB_URI,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex:true
